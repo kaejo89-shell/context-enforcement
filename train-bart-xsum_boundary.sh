6ad5d932ac09cae1828e8ec8a29fe6ccb19c60f5
kaeflint <esseljojo1990@gmail.com>
2023-05-26 09:38:27 +0900

    Modified the code

---
+++ train-bart-xsum_boundary.sh
@@ -0,0 +1,17 @@
+nohup accelerate launch context_enforcement/trainers/train_bart.py \
+--max-seq-len 800 \
+--context-max-len 200 \
+--task-type xsum \
+--num-train-epochs 10 \
+--eval-steps 1000 \
+--lr-scheduler-type linear \
+--learning-rate 5e-5 \
+--warmup-ratio 0.25 \
+--per-device-train-batch-size 12 \
+--per-device-eval-batch-size 12 \
+--save-total-limit 1 \
+--model-base  facebook/bart-base \
+--run-id bart_base_model_context_enforcer \
+--fp16 \
+--gradient-accumulation-steps 4 \
+--output-dir trained_models_sum_boundary/  >> training_logs_bart_base_model_context_enforcer.out &
